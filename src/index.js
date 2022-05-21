2536593d3feb53de650ffaebef9647312b42a929
gandori <marcel.stiebing@web.de>
2022-05-21 13:09:05 +0200

    complete overhaul

--- src/index.js
+++ src/index.js
@@ -1,48 +1,46 @@
+const pwd = document.getElementById("pwd");
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const punctuation = "!§$%&/()=?{}[]";
+
+var dic = "abcdefghijklmnopqrstuvwxyz";
+var interval;
+var p = "";
+var pwd_len = document.getElementById("pwd_len");
+var max = 128;
+var min = 8;
+
 generate();
-document.getElementById("upper_letters").addEventListener("change" ,() => {generate()});
-document.getElementById("numbers").addEventListener("change" ,() => {generate()});
-document.getElementById("punctuation").addEventListener("change" ,() => {generate()});
-document.getElementById("pwd_lenght").addEventListener("change" ,() => {generate()});
 
-function pwd_lenght_change(){
-    const pwd_lenght = document.getElementById("pwd_lenght");
-    var pwd_lenght_label = document.getElementById("pwd_lenght_label");
-    pwd_lenght_label.innerHTML = pwd_lenght.value;
+function dec_pwd_len()
+{
+	if(pwd_len.value >= min){pwd_len.value--;}
+	if(pwd_len.value <= min){pwd_len.value = min;}
 }
 
-function generate(){
-    const password_label = document.getElementById("password");
-    const password_lenght = document.getElementById("pwd_lenght");
-    
-    var password = "";
-
-    var dic = ["abcdefghijklmnopqrstuvwxyz"];
-    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const numbers = "123456789";
-    const punctuation = "!§$%&/()=?{}[]";
-
-    if (document.getElementById("upper_letters").checked){
-        dic.push(letters);
-    }
-    if (document.getElementById("numbers").checked){
-        dic.push(numbers);
-    }
-    if (document.getElementById("punctuation").checked){
-        dic.push(punctuation);
-    }    
-
-    for (let i = 0; i < password_lenght.value; i++){
-        let index = dic[Math.floor(Math.random() * dic.length)];        
-        password = password + index[Math.floor(Math.random() * index.length)];
-    }
-
-    password_label.innerHTML = password;
+function inc_pwd_len()
+{
+	if(pwd_len.value <= max){pwd_len.value++;}
+	if(pwd_len.value >= max){pwd_len.value = max;}
 }
 
-function copy() {
-    const text = document.getElementById("password").innerHTML;
-    if (text != ""){
-        navigator.clipboard.writeText(text);
-        alert("Copied the text: " + text);
-    }
+function generate()
+{
+	if(pwd_len.value >= max){pwd_len.value = max;}
+	if(pwd_len.value === ""){pwd_len.value = min;}
+	if(document.getElementById("upper_letters").checked){dic = dic + letters;}
+    if(document.getElementById("numbers").checked){dic = dic + numbers;}
+    if(document.getElementById("punctuation").checked){dic = dic + punctuation;}
+
+	p = "";
+    for(let i = 0; i < pwd_len.value; i++)
+    {p = p + dic[Math.floor(Math.random() * dic.length)];}
+    pwd.innerHTML = p;
+}
+
+function copy_to_clipboard()
+{
+	if(p == ""){return;}
+    navigator.clipboard.writeText(p);
+    alert("Copied the Password to clipboard: " + p);
 }
